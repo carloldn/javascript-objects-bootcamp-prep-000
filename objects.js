e7a4eeb186c0827265e9fbede5862026b6b9a0de
carlo montanaro <carloldn@outlook.com>
2018-09-23 14:27:42 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,8 +1,7 @@
 var playlist = {
   Kelly: "mornings", Jenna: "nights"
 };
-function updatePlaylist [playlist, artistname, songtitle]{ 
-return playlist.assign ( {},playlist, {[artistname]:songtitle)
+playlist.assign ( {},playlist, {[artistname]:songtitle)}
 removeFromPlaylist (playlist, artistName);
   delete playlist [artistName];
   return updatedPlaylist;
